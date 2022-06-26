508f4e2bee2bee39ec1423f2ff928a2d66e7811f
abdullahiaa <osuolaleabdullahi@gmail.com>
2022-06-26 10:29:08 +0100

    env all set

--- db/db.config.js
+++ db/db.config.js
@@ -2,10 +2,10 @@ const { Sequelize } = require("sequelize");
 const dontenv = require("dotenv");
 dontenv.config();
 
-const { DB_USERNAME, DB_PASSWORD, DB_HOST, NODE_ENV } = process.env;
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize("swiftfood", DB_USERNAME, DB_PASSWORD, {
-  host: NODE_ENV === "production" ? DB_HOST : "127.0.0.1",
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "mysql",
 });
 
